1a014ada64e3414ee9ba232d4925b176950e110a
John Ragan <jpragan16@gmail.com>
2015-04-15 18:52:54 -0700

    Update Cell.java

--- FinalProject/Reversi/src/Cell.java
+++ FinalProject/Reversi/src/Cell.java
@@ -1,23 +1,46 @@
-
-public class Cell {
+public enum Cell {
 	
+		BLACK('B'),WHITE('W'),EMPTY('E');
+		private char color;
 
+		private Cell(char color) 
+		{
+			this.color = color;
+			
+		}
+
+		protected char getColor()
+		{
+			return this.color;
+		}
+		
+		public void flipColor(Cell c)
+		{
+			if (this.color == 'B')
+				this.color = 'W';
+
+			if (this.color == 'W')
+				this.color = 'B';
+
+		//	if (this.color == 'E')
+		//		throw  EmptySpaceException();
+		}
+		
+		public boolean equals(Cell c)
+		{
+			if ( this.color == c.color )
+				return true;
+			return false;
+		}
+
+		public String toString()
+		{
+			switch(this){
+			case BLACK: return "B";
+			case WHITE: return "W";
+			case EMPTY: return "E";
+			default: throw new IllegalArgumentException(); // had to have an exception to get this to compile. I will catch it later in the code.
+			}		
+		}
 	
-	private Chip place = Chip.EMPTY;
-	
-	public Cell()
-	{
-		Chip place = Chip.EMPTY;
-	}
-	public Cell(Chip C)
-	{
-		this.place = C;  
-	}
-
-	public String toString()
-	{
-		return place.toString();
-	}
-}
-
-
+};
