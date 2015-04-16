baf398ea1ce73e47a4e69242d7a42c7a8882eef3
John Ragan <jpragan16@gmail.com>
2015-04-15 18:53:18 -0700

    Update BoardTest.java

--- FinalProject/Reversi/src/BoardTest.java
+++ FinalProject/Reversi/src/BoardTest.java
@@ -5,7 +5,16 @@ public class BoardTest {
 		Board board = new Board();
 		board.initialize();
 		System.out.println(board);
-
+		
+		System.out.println(board.isBlack(0, 0));
+		System.out.println(board.isWhite(0, 0));
+		System.out.println(board.isWhite(1, 5));
+		System.out.println(board.isBlack(1, 5));
+		System.out.println(board.isEmpty(1, 1));
+		
+		
+	
+		
 	}
 
 }
